aa50a9515cf2560c198f23f4c8904d9a7c58a15c
Super Vision <107816707+aARdeLife@users.noreply.github.com>
2023-04-20 20:36:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const video = document.getElementById('video');
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-const loader = new THREE.GLTFLoader();
 
 async function setupCamera() {
     const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' }, audio: false });
@@ -17,12 +16,6 @@ function isPointInRect(x, y, rect) {
     return x >= rect[0] && x <= rect[0] + rect[2] && y >= rect[1] && y <= rect[1] + rect[3];
 }
 
-async function load3DModel(url) {
-    return new Promise((resolve, reject) => {
-        loader.load(url, resolve, undefined, reject);
-    });
-}
-
 canvas.addEventListener('click', async event => {
     const rect = canvas.getBoundingClientRect();
     const x = event.clientX - rect.left;
@@ -30,21 +23,15 @@ canvas.addEventListener('click', async event => {
 
     for (const prediction of currentPredictions) {
         if (isPointInRect(x, y, prediction.bbox)) {
-            try {
-                const gltf = await load3DModel('https://github.com/aARdeLife/SuperVision/blob/6e7ab011f51b2483b72237297fbffd5e27a470cc/polaris/polforweb%20(3).glb?raw=true');
-                // Render the 3D model over the detected object
-                render3DModel(gltf, prediction.bbox);
-            } catch (error) {
-                console.error('Error loading 3D model:', error);
-            }
+            // Render the 3D model over the detected object
+            render3DModel(prediction.bbox);
             return;
         }
     }
 });
 
-function render3DModel(gltf, bbox) {
-    // Save the GLTF data and bounding box as JSON in sessionStorage
-    sessionStorage.setItem('gltfData', JSON.stringify(gltf));
+function render3DModel(bbox) {
+    // Save the bounding box as JSON in sessionStorage
     sessionStorage.setItem('objectBbox', JSON.stringify(bbox));
 
     // Open the 3dmodel.html file in a new window
@@ -79,3 +66,4 @@ async function detectObjects() {
     videoElement.play();
     detectObjects();
 })();
+
