0d19907a79262cb59801b1ff80691e62134817f6
Super Vision <107816707+aARdeLife@users.noreply.github.com>
2023-04-20 21:53:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -23,18 +23,25 @@ canvas.addEventListener('click', async event => {
 
     for (const prediction of currentPredictions) {
         if (isPointInRect(x, y, prediction.bbox)) {
-            // Render the 3D model over the detected object
+            displayObjectInfo(prediction);
             render3DModel(prediction.bbox);
             return;
         }
     }
 });
 
-function render3DModel(bbox) {
-    // Save the bounding box as JSON in sessionStorage
-    sessionStorage.setItem('objectBbox', JSON.stringify(bbox));
+async function displayObjectInfo(prediction) {
+    const objectName = prediction.class;
+    const objectInfo = await fetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${objectName}`)
+        .then(response => response.json())
+        .then(data => data.extract);
 
-    // Open the 3dmodel.html file in a new window
+    alert(`${objectName}: ${objectInfo}`);
+}
+
+function render3DModel(bbox) {
+    sessionStorage.setItem('objectBbox', JSON.stringify(bbox));
+    sessionStorage.setItem('modelURL', 'https://github.com/aARdeLife/SuperVision/blob/6e7ab011f51b2483b72237297fbffd5e27a470cc/polaris/polforweb%20(3).glb');
     window.open('file:///C:/Users/Shadow/Desktop/Super%20Vision%20XR/3dmodel.html');
 }
 
@@ -43,7 +50,6 @@ async function detectObjects() {
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
 
-    // Initialize currentPredictions as an empty array
     currentPredictions = [];
     
     while (true) {
@@ -56,6 +62,17 @@ async function detectObjects() {
             ctx.lineWidth = 4;
             ctx.strokeRect(...prediction.bbox);
         });
+
+        await tf.nextFrame();
+    }
+}
+
+(async function() {
+    const videoElement = await setupCamera();
+    videoElement.play();
+    detectObjects();
+})();
+
 
         await tf.nextFrame();
     }
