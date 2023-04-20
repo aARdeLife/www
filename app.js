02cb1dfecffdfa98b151c1a5f8181feb798d081d
Super Vision <107816707+aARdeLife@users.noreply.github.com>
2023-04-21 01:11:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,72 +1,81 @@
-const videoElement = document.getElementById('video');
-const canvasElement = document.getElementById('canvas');
-const ctx = canvasElement.getContext('2d');
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
 const tooltip = document.getElementById('tooltip');
 
-async function setupCamera() {
-    const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' }, audio: false });
-    videoElement.srcObject = stream;
-    videoElement.width = window.innerWidth;
-    videoElement.height = window.innerHeight;
-    canvasElement.width = videoElement.width;
-    canvasElement.height = videoElement.height;
-    return new Promise(resolve => {
-        videoElement.onloadedmetadata = () => {
-            resolve(videoElement);
-        };
-    });
+let model;
+
+async function loadModel() {
+    model = await cocoSsd.load();
+}
+
+function startVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then((stream) => {
+            video.srcObject = stream;
+            video.addEventListener('loadeddata', () => {
+                canvas.width = video.videoWidth;
+                canvas.height = video.videoHeight;
+                detectObjects();
+            });
+        });
 }
 
 async function detectObjects() {
-    const net = await cocoSsd.load();
-    while (true) {
-        const predictions = await net.detect(videoElement);
+    if (model) {
+        const predictions = await model.detect(video);
+        renderPredictions(predictions);
+    }
+    requestAnimationFrame(detectObjects);
+}
 
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+function renderPredictions(predictions) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
 
-        ctx.drawImage(videoElement, 0, 0, ctx.canvas.width, ctx.canvas.height);
+    predictions.forEach((prediction) => {
+        const bbox = prediction.bbox;
+        context.strokeStyle = 'green';
+        context.lineWidth = 2;
+        context.strokeRect(bbox[0], bbox[1], bbox[2], bbox[3]);
 
-        predictions.forEach(prediction => {
-            ctx.strokeStyle = '#00FF00';
-            ctx.lineWidth = 4;
-            ctx.strokeRect(...prediction.bbox);
-        });
+        context.fillStyle = 'green';
+        context.font = '12px Arial';
+        context.fillText(prediction.class, bbox[0], bbox[1] - 5);
+    });
 
-        canvasElement.onmousemove = (e) => {
-            const mouseX = e.clientX;
-            const mouseY = e.clientY;
-
-            const prediction = predictions.find(p => {
-                const [x, y, width, height] = p.bbox;
-                return mouseX >= x && mouseX <= x + width && mouseY >= y && mouseY <= y + height;
-            });
-
-            if (prediction) {
+    canvas.addEventListener('mousemove', (event) => {
+        predictions.forEach((prediction) => {
+            const [x, y, width, height] = prediction.bbox;
+            if (
+                event.offsetX > x &&
+                event.offsetX < x + width &&
+                event.offsetY > y &&
+                event.offsetY < y + height
+            ) {
+                tooltip.innerHTML = `<strong>${prediction.class}</strong><br>Confidence: ${(prediction.score * 100).toFixed(2)}%`;
+                tooltip.style.left = `${event.pageX + 10}px`;
+                tooltip.style.top = `${event.pageY}px`;
                 tooltip.style.display = 'block';
-                tooltip.style.left = `${e.clientX}px`;
-                tooltip.style.top = `${e.clientY}px`;
-                tooltip.textContent = `${prediction.class}`;
             } else {
                 tooltip.style.display = 'none';
             }
-        };
+        });
+    });
 
-        canvasElement.onclick = async (e) => {
-            const mouseX = e.clientX;
-            const mouseY = e.clientY;
-
-            const prediction = predictions.find(p => {
-                const [x, y, width, height] = p.bbox;
-                return mouseX >= x && mouseX <= x + width && mouseY >= y && mouseY <= y + height;
-            });
-
-            if (prediction) {
-                await render3DModel();
+    canvas.addEventListener('click', (event) => {
+        predictions.forEach((prediction) => {
+            const [x, y, width, height] = prediction.bbox;
+            if (
+                event.offsetX > x &&
+                event.offsetX < x + width &&
+                event.offsetY > y &&
+                event.offsetY < y + height
+            ) {
+                render3DModel();
             }
-        };
-
-        await tf.nextFrame();
-    }
+        });
+    });
 }
 
 async function render3DModel() {
@@ -77,26 +86,26 @@ async function render3DModel() {
     scene.setAttribute('arjs', 'sourceType: webcam; debugUIEnabled: false;');
     scene.setAttribute('vr-mode-ui', 'enabled: false');
 
-    const plane = document.createElement('a-plane');
-    plane.setAttribute('position', '0 0 -4');
-    plane.setAttribute('rotation', '-90 0 0');
-    plane.setAttribute('width', '2');
-    plane.setAttribute('height', '2');
-    plane.setAttribute('color', '#7BC8A4');
-    scene.appendChild(plane);
+    const camera = document.createElement('a-entity');
+    camera.setAttribute('gps-camera', '');
+    camera.setAttribute('rotation-reader', '');
+    scene.appendChild(camera);
+
+    // Replace the latitude and longitude values with the coordinates where you want the 3D model to appear.
+    const latitude = 0; // Your latitude here
+    const longitude = 0; // Your longitude here
 
     const model = document.createElement('a-entity');
     model.setAttribute('gltf-model', `url(${modelURL})`);
     model.setAttribute('scale', '0.05 0.05 0.05');
-    model.setAttribute('position', '0 0.05 0');
-    plane.appendChild(model);
+    model.setAttribute('position', `gps-projected-entity-place_${latitude}_${longitude}`);
+    model.setAttribute('gps-projected-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
+    scene.appendChild(model);
 
     document.body.appendChild(scene);
 }
 
-(async function () {
-    const videoElement = await setupCamera();
-    videoElement.play();
-    detectObjects();
-})();
+loadModel().then(() => {
+    startVideo();
+});
 
